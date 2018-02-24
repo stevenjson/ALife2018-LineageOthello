e5838c567ba081859107b33c26810147f5ee9b68
Alex Lalejini <amlalejini@gmail.com>
2018-02-24 13:47:17 -0500

    Moved instruction lib configuration over to instruction implementation header. Added instruction descriptions to SGP instructions.

--- source/LineageExp.h
+++ source/LineageExp.h
@@ -652,7 +652,9 @@ public:
 
   // Config functions. These do all of the hardware-specific experiment setup/configuration.
   void ConfigSGP();
+  void ConfigSGP_InstLib();
   void ConfigAGP();
+  void ConfigAGP_InstLib();
 
   // Mutation functions
   size_t SGP__Mutate(SignalGPAgent & agent, emp::Random & rnd);
@@ -1036,120 +1038,7 @@ void LineageExp::ConfigSGP() {
     genotype->GetData().RecordMutation(last_mutation);
   });
 
-  // Configure the instruction set.
-  // - Default instruction set.
-  sgp_inst_lib->AddInst("Inc", SGP__hardware_t::Inst_Inc, 1, "Increment value in local memory Arg1");
-  sgp_inst_lib->AddInst("Dec", SGP__hardware_t::Inst_Dec, 1, "Decrement value in local memory Arg1");
-  sgp_inst_lib->AddInst("Not", SGP__hardware_t::Inst_Not, 1, "Logically toggle value in local memory Arg1");
-  sgp_inst_lib->AddInst("Add", SGP__hardware_t::Inst_Add, 3, "Local memory: Arg3 = Arg1 + Arg2");
-  sgp_inst_lib->AddInst("Sub", SGP__hardware_t::Inst_Sub, 3, "Local memory: Arg3 = Arg1 - Arg2");
-  sgp_inst_lib->AddInst("Mult", SGP__hardware_t::Inst_Mult, 3, "Local memory: Arg3 = Arg1 * Arg2");
-  sgp_inst_lib->AddInst("Div", SGP__hardware_t::Inst_Div, 3, "Local memory: Arg3 = Arg1 / Arg2");
-  sgp_inst_lib->AddInst("Mod", SGP__hardware_t::Inst_Mod, 3, "Local memory: Arg3 = Arg1 % Arg2");
-  sgp_inst_lib->AddInst("TestEqu", SGP__hardware_t::Inst_TestEqu, 3, "Local memory: Arg3 = (Arg1 == Arg2)");
-  sgp_inst_lib->AddInst("TestNEqu", SGP__hardware_t::Inst_TestNEqu, 3, "Local memory: Arg3 = (Arg1 != Arg2)");
-  sgp_inst_lib->AddInst("TestLess", SGP__hardware_t::Inst_TestLess, 3, "Local memory: Arg3 = (Arg1 < Arg2)");
-  sgp_inst_lib->AddInst("If", SGP__hardware_t::Inst_If, 1, "Local memory: If Arg1 != 0, proceed; else, skip block.", emp::ScopeType::BASIC, 0, {"block_def"});
-  sgp_inst_lib->AddInst("While", SGP__hardware_t::Inst_While, 1, "Local memory: If Arg1 != 0, loop; else, skip block.", emp::ScopeType::BASIC, 0, {"block_def"});
-  sgp_inst_lib->AddInst("Countdown", SGP__hardware_t::Inst_Countdown, 1, "Local memory: Countdown Arg1 to zero.", emp::ScopeType::BASIC, 0, {"block_def"});
-  sgp_inst_lib->AddInst("Close", SGP__hardware_t::Inst_Close, 0, "Close current block if there is a block to close.", emp::ScopeType::BASIC, 0, {"block_close"});
-  sgp_inst_lib->AddInst("Break", SGP__hardware_t::Inst_Break, 0, "Break out of current block.");
-  sgp_inst_lib->AddInst("Call", SGP__hardware_t::Inst_Call, 0, "Call function that best matches call affinity.", emp::ScopeType::BASIC, 0, {"affinity"});
-  sgp_inst_lib->AddInst("Return", SGP__hardware_t::Inst_Return, 0, "Return from current function if possible.");
-  sgp_inst_lib->AddInst("SetMem", SGP__hardware_t::Inst_SetMem, 2, "Local memory: Arg1 = numerical value of Arg2");
-  sgp_inst_lib->AddInst("CopyMem", SGP__hardware_t::Inst_CopyMem, 2, "Local memory: Arg1 = Arg2");
-  sgp_inst_lib->AddInst("SwapMem", SGP__hardware_t::Inst_SwapMem, 2, "Local memory: Swap values of Arg1 and Arg2.");
-  sgp_inst_lib->AddInst("Input", SGP__hardware_t::Inst_Input, 2, "Input memory Arg1 => Local memory Arg2.");
-  sgp_inst_lib->AddInst("Output", SGP__hardware_t::Inst_Output, 2, "Local memory Arg1 => Output memory Arg2.");
-  sgp_inst_lib->AddInst("Commit", SGP__hardware_t::Inst_Commit, 2, "Local memory Arg1 => Shared memory Arg2.");
-  sgp_inst_lib->AddInst("Pull", SGP__hardware_t::Inst_Pull, 2, "Shared memory Arg1 => Shared memory Arg2.");
-  sgp_inst_lib->AddInst("Nop", SGP__hardware_t::Inst_Nop, 0, "No operation.");
-  // - Non-default instruction set.
-  // TODO (@amlalejini): Fill out instruction descriptions.
-  sgp_inst_lib->AddInst("Fork",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_Fork(hw, inst); },
-                        0, "...");
-  sgp_inst_lib->AddInst("GetBoardWidth",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP_Inst_GetBoardWidth(hw, inst); },
-                        1, "...");
-  sgp_inst_lib->AddInst("EndTurn",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP_Inst_EndTurn(hw, inst); },
-                        0, "...");
-  sgp_inst_lib->AddInst("SetMoveXY",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_SetMoveXY(hw, inst); },
-                        2, "...");
-  sgp_inst_lib->AddInst("SetMoveID",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_SetMoveID(hw, inst); },
-                        1, "...");
-  sgp_inst_lib->AddInst("GetMoveXY",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_GetMoveXY(hw, inst); },
-                        2, "...");
-  sgp_inst_lib->AddInst("GetMoveID",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_GetMoveID(hw, inst); },
-                        1, "...");
-  sgp_inst_lib->AddInst("IsValidXY-HW",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_IsValidXY_HW(hw, inst); },
-                        3, "...");
-  sgp_inst_lib->AddInst("IsValidID-HW",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_IsValidID_HW(hw, inst); },
-                        2, "...");
-  sgp_inst_lib->AddInst("IsValidOppXY-HW",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_IsValidOppXY_HW(hw, inst); },
-                        3, "...");
-  sgp_inst_lib->AddInst("IsValidOppID-HW",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_IsValidOppID_HW(hw, inst); },
-                        2, "...");
-  sgp_inst_lib->AddInst("AdjacentXY",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_AdjacentXY(hw, inst); },
-                        3, "...");
-  sgp_inst_lib->AddInst("AdjacentID",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_AdjacentID(hw, inst); },
-                        2, "...");
-  sgp_inst_lib->AddInst("ValidMoveCnt-HW",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_ValidMoveCnt_HW(hw, inst); },
-                        1, "...");
-  sgp_inst_lib->AddInst("ValidOppMoveCnt-HW",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_ValidOppMoveCnt_HW(hw, inst); },
-                        1, "...");
-  sgp_inst_lib->AddInst("GetBoardValueXY-HW",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_GetBoardValueXY_HW(hw, inst); },
-                        3, "...");
-  sgp_inst_lib->AddInst("GetBoardValueID-HW",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_GetBoardValueID_HW(hw, inst); },
-                        2, "...");
-  sgp_inst_lib->AddInst("PlaceDiskXY-HW",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_PlaceDiskXY_HW(hw, inst); },
-                        3, "...");
-  sgp_inst_lib->AddInst("PlaceDiskID-HW",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_PlaceDiskID_HW(hw, inst); },
-                        2, "...");
-  sgp_inst_lib->AddInst("PlaceOppDiskXY-HW",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_PlaceOppDiskXY_HW(hw, inst); },
-                        3, "...");
-  sgp_inst_lib->AddInst("PlaceOppDiskID-HW",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_PlaceOppDiskID_HW(hw, inst); },
-                        2, "...");
-  sgp_inst_lib->AddInst("FlipCntXY-HW",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_FlipCntXY_HW(hw, inst); },
-                        3, "...");
-  sgp_inst_lib->AddInst("FlipCntID-HW",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_FlipCntID_HW(hw, inst); },
-                        2, "...");
-  sgp_inst_lib->AddInst("OppFlipCntXY-HW",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_OppFlipCntXY_HW(hw, inst); },
-                        3, "...");
-  sgp_inst_lib->AddInst("OppFlipCntID-HW",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_OppFlipCntID_HW(hw, inst); },
-                        2, "...");
-  sgp_inst_lib->AddInst("FrontierCnt-HW",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_FrontierCnt_HW(hw, inst); },
-                        3, "...");
-  sgp_inst_lib->AddInst("ResetBoard-HW",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_ResetBoard_HW(hw, inst); },
-                        0, "...");
-  sgp_inst_lib->AddInst("IsOver-HW",
-                        [this](SGP__hardware_t & hw, const SGP__inst_t & inst) { this->SGP__Inst_IsOver_HW(hw, inst); },
-                        1, "...");
+  ConfigSGP_InstLib();
 
   sgp_eval_hw = emp::NewPtr<SGP__hardware_t>(sgp_inst_lib, sgp_event_lib, random);
   sgp_eval_hw->SetMinBindThresh(SGP_HW_MIN_BIND_THRESH);
@@ -1378,115 +1267,7 @@ void LineageExp::ConfigAGP() {
     genotype->GetData().RecordMutation(last_mutation);
   });
 
-  agp_inst_lib->AddInst("Dec", AGP__inst_lib_t::Inst_Dec, 1, "Decrement value in reg Arg1");
-  agp_inst_lib->AddInst("Not", AGP__inst_lib_t::Inst_Not, 1, "Logically toggle value in reg Arg1");
-  agp_inst_lib->AddInst("SetReg", AGP__inst_lib_t::Inst_SetReg, 2, "Set reg Arg1 to numerical value Arg2");
-  agp_inst_lib->AddInst("Add", AGP__inst_lib_t::Inst_Add, 3, "regs: Arg3 = Arg1 + Arg2");
-  agp_inst_lib->AddInst("Sub", AGP__inst_lib_t::Inst_Sub, 3, "regs: Arg3 = Arg1 - Arg2");
-  agp_inst_lib->AddInst("Mult", AGP__inst_lib_t::Inst_Mult, 3, "regs: Arg3 = Arg1 * Arg2");
-  agp_inst_lib->AddInst("Div", AGP__inst_lib_t::Inst_Div, 3, "regs: Arg3 = Arg1 / Arg2");
-  agp_inst_lib->AddInst("Inc", AGP__inst_lib_t::Inst_Inc, 1, "Increment value in reg Arg1");
-  agp_inst_lib->AddInst("Mod", AGP__inst_lib_t::Inst_Mod, 3, "regs: Arg3 = Arg1 % Arg2");
-  agp_inst_lib->AddInst("TestEqu", AGP__inst_lib_t::Inst_TestEqu, 3, "regs: Arg3 = (Arg1 == Arg2)");
-  agp_inst_lib->AddInst("TestNEqu", AGP__inst_lib_t::Inst_TestNEqu, 3, "regs: Arg3 = (Arg1 != Arg2)");
-  agp_inst_lib->AddInst("TestLess", AGP__inst_lib_t::Inst_TestLess, 3, "regs: Arg3 = (Arg1 < Arg2)");
-  agp_inst_lib->AddInst("If", AGP__inst_lib_t::Inst_If, 2, "If reg Arg1 != 0, scope -> Arg2; else skip scope", emp::ScopeType::BASIC, 1);
-  agp_inst_lib->AddInst("While", AGP__inst_lib_t::Inst_While, 2, "Until reg Arg1 != 0, repeat scope Arg2; else skip", emp::ScopeType::LOOP, 1);
-  agp_inst_lib->AddInst("Countdown", AGP__inst_lib_t::Inst_Countdown, 2, "Countdown reg Arg1 to zero; scope to Arg2", emp::ScopeType::LOOP, 1);
-  agp_inst_lib->AddInst("Break", AGP__inst_lib_t::Inst_Break, 1, "Break out of scope Arg1");
-  agp_inst_lib->AddInst("Scope", AGP__inst_lib_t::Inst_Scope, 1, "Enter scope Arg1", emp::ScopeType::BASIC, 0);
-  agp_inst_lib->AddInst("Define", AGP__inst_lib_t::Inst_Define, 2, "Build function Arg1 in scope Arg2", emp::ScopeType::FUNCTION, 1);
-  agp_inst_lib->AddInst("Call", AGP__inst_lib_t::Inst_Call, 1, "Call previously defined function Arg1");
-  agp_inst_lib->AddInst("Push", AGP__inst_lib_t::Inst_Push, 2, "Push reg Arg1 onto stack Arg2");
-  agp_inst_lib->AddInst("Pop", AGP__inst_lib_t::Inst_Pop, 2, "Pop stack Arg1 into reg Arg2");
-  agp_inst_lib->AddInst("Input", AGP__inst_lib_t::Inst_Input, 2, "Pull next value from input Arg1 into reg Arg2");
-  agp_inst_lib->AddInst("Output", AGP__inst_lib_t::Inst_Output, 2, "Push reg Arg1 into output Arg2");
-  agp_inst_lib->AddInst("CopyVal", AGP__inst_lib_t::Inst_CopyVal, 2, "Copy reg Arg1 into reg Arg2");
-  agp_inst_lib->AddInst("ScopeReg", AGP__inst_lib_t::Inst_ScopeReg, 1, "Backup reg Arg1; restore at end of scope");
-  agp_inst_lib->AddInst("Nop", [](AGP__hardware_t & hw, const AGP__inst_t & inst){ ; }, 0, "No operation.");
-
-  // - Non-default instruction set.
-  agp_inst_lib->AddInst("GetBoardWidth",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_GetBoardWidth(hw, inst); },
-                        1, "...");
-  agp_inst_lib->AddInst("EndTurn",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_EndTurn(hw, inst); },
-                        0, "...");
-  agp_inst_lib->AddInst("SetMoveXY",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_SetMoveXY(hw, inst); },
-                        2, "...");
-  agp_inst_lib->AddInst("SetMoveID",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_SetMoveID(hw, inst); },
-                        1, "...");
-  agp_inst_lib->AddInst("GetMoveXY",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_GetMoveXY(hw, inst); },
-                        2, "...");
-  agp_inst_lib->AddInst("GetMoveID",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_GetMoveID(hw, inst); },
-                        1, "...");
-  agp_inst_lib->AddInst("IsValidOppXY-HW",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_IsValidOppXY_HW(hw, inst); },
-                        3, "...");
-  agp_inst_lib->AddInst("IsValidOppID-HW",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_IsValidOppID_HW(hw, inst); },
-                        2, "...");
-  agp_inst_lib->AddInst("IsValidXY-HW",
-                        [this](AGP__hardware_t &hw, const AGP__inst_t &inst) { this->AGP__Inst_IsValidXY_HW(hw, inst); },
-                        3, "...");
-  agp_inst_lib->AddInst("IsValidID-HW",
-                        [this](AGP__hardware_t &hw, const AGP__inst_t &inst) { this->AGP__Inst_IsValidID_HW(hw, inst); },
-                        2, "...");
-  agp_inst_lib->AddInst("AdjacentXY",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_AdjacentXY(hw, inst); },
-                        3, "...");
-  agp_inst_lib->AddInst("AdjacentID",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_AdjacentID(hw, inst); },
-                        2, "...");
-  agp_inst_lib->AddInst("ValidMoveCnt-HW",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_ValidMoveCnt_HW(hw, inst); },
-                        1, "...");
-  agp_inst_lib->AddInst("ValidOppMoveCnt-HW",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_ValidOppMoveCnt_HW(hw, inst); },
-                        1, "...");
-  agp_inst_lib->AddInst("GetBoardValueXY-HW",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_GetBoardValueXY_HW(hw, inst); },
-                        3, "...");
-  agp_inst_lib->AddInst("GetBoardValueID-HW",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_GetBoardValueID_HW(hw, inst); },
-                        2, "...");
-  agp_inst_lib->AddInst("PlaceDiskXY-HW",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_PlaceDiskXY_HW(hw, inst); },
-                        3, "...");
-  agp_inst_lib->AddInst("PlaceDiskID-HW",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_PlaceDiskID_HW(hw, inst); },
-                        2, "...");
-  agp_inst_lib->AddInst("PlaceOppDiskXY-HW",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_PlaceOppDiskXY_HW(hw, inst); },
-                        3, "...");
-  agp_inst_lib->AddInst("PlaceOppDiskID-HW",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_PlaceOppDiskID_HW(hw, inst); },
-                        2, "...");
-  agp_inst_lib->AddInst("FlipCntXY-HW",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_FlipCntXY_HW(hw, inst); },
-                        3, "...");
-  agp_inst_lib->AddInst("FlipCntID-HW",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_FlipCntID_HW(hw, inst); },
-                        2, "...");
-  agp_inst_lib->AddInst("OppFlipCntXY-HW",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_OppFlipCntXY_HW(hw, inst); },
-                        3, "...");
-  agp_inst_lib->AddInst("OppFlipCntID-HW",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_OppFlipCntID_HW(hw, inst); },
-                        2, "...");
-  agp_inst_lib->AddInst("FrontierCnt-HW",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_FrontierCnt_HW(hw, inst); },
-                        1, "...");
-  agp_inst_lib->AddInst("ResetBoard-HW",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_ResetBoard_HW(hw, inst); },
-                        0, "...");
-  agp_inst_lib->AddInst("IsOver-HW",
-                        [this](AGP__hardware_t & hw, const AGP__inst_t & inst) { this->AGP__Inst_IsOver_HW(hw, inst); },
-                        1, "...");
+  ConfigAGP_InstLib();
 
   agp_eval_hw = emp::NewPtr<AGP__hardware_t>(agp_inst_lib);
 
