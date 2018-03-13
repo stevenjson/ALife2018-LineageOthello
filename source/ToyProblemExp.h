60c2cefc08f5e9d465e623198042351bac3b2fd5
Alex Lalejini <amlalejini@gmail.com>
2018-03-13 17:03:40 -0400

    General updates to toy problem. Added back in systematics.

--- source/ToyProblemExp.h
+++ source/ToyProblemExp.h
@@ -30,14 +30,13 @@
 #include "cec2013.h"
 
 // TODO:
-// [ ] How do we want to do mutation/phenotype reporting to systematics?
+// [ ] full lineage snapshots (~500 updates)
+//    - Update PrintLineage to print count
 
 constexpr size_t RUN_ID__EXP = 0;
 constexpr size_t RUN_ID__ANALYSIS = 1;
 
 constexpr size_t DIMENSIONS = 2;
-
-constexpr double MIN_DIST = 0.000001;
 
 constexpr size_t SELECTION_METHOD_ID__TOURNAMENT = 0;
 constexpr size_t SELECTION_METHOD_ID__LEXICASE = 1;
@@ -45,8 +44,7 @@ constexpr size_t SELECTION_METHOD_ID__ECOEA = 2;
 constexpr size_t SELECTION_METHOD_ID__MAPELITES = 3;
 constexpr size_t SELECTION_METHOD_ID__ROULETTE = 4;
 
-
-const emp::vector<std::string> MUTATION_TYPES = {"inst_substitutions", "arg_substitutions", "tag_bit_flips", "inst_insertions", "inst_deletions", "func_duplications", "func_deletions"};
+const emp::vector<std::string> MUTATION_TYPES = {"normal", "normal_x", "normal_y"};
 
 // Available problems: (1-indexed in cpp library)
 // 0) F4 (2D)  --> Index: 4
@@ -101,8 +99,10 @@ public:
   };
 
   using phenotype_t = emp::vector<double>;
-  using data_t = emp::mut_landscape_info<phenotype_t>; //TODO: ask Emily about this.
+  using mut_count_t = std::unordered_map<std::string, double>;
+  using data_t = emp::mut_landscape_info<phenotype_t>;
   using world_t = emp::World<Agent, data_t>;
+  using genotype_t = world_t::genotype_t;
 
 protected:
   size_t RUN_MODE;
@@ -123,7 +123,7 @@ protected:
   double RESOURCE_SELECT__MAX_BONUS;
   double RESOURCE_SELECT__COST;
 
-  double MUTATION_STD;
+  double MUTATION_STD;    ///< Given as ratio of dimension domain.
 
   size_t SYSTEMATICS_INTERVAL;
   size_t FITNESS_INTERVAL;
@@ -136,19 +136,21 @@ protected:
   size_t update;
   size_t best_agent_id;
 
-  //TODO: lexicase support
   emp::vector<std::function<double(Agent &)>> fit_set;
   emp::vector<emp::Resource> resources;                 ///< Resources for emp::ResourceSelect.
   emp::vector<emp::vector<double>> key_points;
   emp::vector<double> mid_point;
   emp::vector<double> ubounds;
   emp::vector<double> lbounds;
+  emp::vector<double> mut_std;
   double max_dist;
   double min_score;
 
+  double score_ceil;
+  double score_floor;
 
   emp::vector<Phenotype> agent_phen_cache;
-
+  mut_count_t last_mutation;
 
   emp::Ptr<world_t> world;
 
@@ -161,8 +163,8 @@ protected:
   emp::Signal<void(void)> do_analysis_sig;          ///< Triggered if in analysis mode. Should trigger appropriate analyses.
 
   // Systematics-specific signals.
-  emp::Signal<void(size_t)> do_pop_snapshot_sig;                ///< Triggered if we should take a snapshot of the population (as defined by POP_SNAPSHOT_INTERVAL). Should call appropriate functions to take snapshot.
-  emp::Signal<void(size_t pos, double)> record_fit_sig;        ///< Trigger signal before organism gives birth.
+  emp::Signal<void(size_t)> do_pop_snapshot_sig;                       ///< Triggered if we should take a snapshot of the population (as defined by POP_SNAPSHOT_INTERVAL). Should call appropriate functions to take snapshot.
+  emp::Signal<void(size_t pos, double)> record_fit_sig;                ///< Trigger signal before organism gives birth.
   emp::Signal<void(size_t pos, const phenotype_t &)> record_phen_sig;  ///< Trigger signal before organism gives birth.
 
   std::function<void(Agent &)> evaluate_agent; ///< Evaluate agent on everything required for selection. Cache results.
@@ -187,7 +189,7 @@ protected:
 
 public:
   ToyProblemExp(const ToyConfig & config)   // @constructor
-    : min_score(0)
+    : min_score(0), score_ceil(0), score_floor(0)
   {
     RUN_MODE = config.RUN_MODE();
     RANDOM_SEED = config.RANDOM_SEED();
@@ -214,12 +216,15 @@ public:
     POP_SNAPSHOT_INTERVAL = config.POP_SNAPSHOT_INTERVAL();
     DATA_DIRECTORY = config.DATA_DIRECTORY();
 
-    for (size_t i = 0; i < PROBLEM_MAP.size(); ++i) {
-      // std::cout << "Problem: " << PROBLEM_MAP[i] << std::endl;
-      eval_function = emp::NewPtr<CEC2013>(PROBLEM_MAP[i]);
-      // std::cout << "  Dimensions: " << eval_function->get_dimension() << std::endl;
-      // std::cout << "  Eval 1,1: " << eval_function->evaluate({1,1}) << std::endl;
-      eval_function.Delete();
+    // for (size_t i = 0; i < PROBLEM_MAP.size(); ++i) {
+    //   std::cout << "Problem: " << PROBLEM_MAP[i] << std::endl;
+    //   eval_function = emp::NewPtr<CEC2013>(PROBLEM_MAP[i]);
+    //   std::cout << "  Dimensions: " << eval_function->get_dimension() << std::endl;
+    //   eval_function.Delete();
+    // }
+
+    for (size_t i = 0; i < MUTATION_TYPES.size(); ++i) {
+      last_mutation.emplace(MUTATION_TYPES[i], 0);
     }
 
     random = emp::NewPtr<emp::Random>(RANDOM_SEED);
@@ -242,6 +247,7 @@ public:
     mid_point.resize(DIMENSIONS);
     ubounds.resize(DIMENSIONS);
     lbounds.resize(DIMENSIONS);
+    mut_std.resize(DIMENSIONS);
     for (size_t dim = 0; dim < DIMENSIONS; ++dim) {
       grid_anchors[dim].resize(HINT_GRID_RES, 0.0);
       double l = eval_function->get_lbound(dim);
@@ -250,19 +256,24 @@ public:
       mid_point[dim] = l + ((u - l)/2);
       ubounds[dim] = u;
       lbounds[dim] = l;
+      mut_std[dim] = (u - l)*MUTATION_STD;
       for (size_t i = 0; i < HINT_GRID_RES; ++i) {
         grid_anchors[dim][i] = l + (i*step);
       }
     }
     max_dist = CalcDist(ubounds, lbounds);
 
-    // Fill out key_points.
+    // Fill out key_points. (NOTE: currently not generic for more than 2 dimensions...)
+    score_ceil = eval_function->get_fitness_goptima();
+    score_floor = score_ceil;
     for (size_t i = 0; i < HINT_GRID_RES; ++i) {
       for (size_t j = 0; j < HINT_GRID_RES; ++j) {
         key_points.emplace_back();
         key_points.back().resize(DIMENSIONS);
         key_points.back()[0] = grid_anchors[0][i];
         key_points.back()[1] = grid_anchors[1][j];
+        const double key_point_score = eval_function->evaluate(key_points.back());
+        score_floor = (key_point_score < score_floor) ? key_point_score : score_floor;
       }
     }
 
@@ -284,14 +295,14 @@ public:
       sys_file.SetTimingRepeat(SYSTEMATICS_INTERVAL);
       auto & fit_file = world->SetupFitnessFile(DATA_DIRECTORY + "fitness.csv");
       fit_file.SetTimingRepeat(FITNESS_INTERVAL);
-      // emp::AddPhylodiversityFile(*world, DATA_DIRECTORY + "phylodiversity.csv").SetTimingRepeat(SYSTEMATICS_INTERVAL);
-      // emp::AddLineageMutationFile(*world, DATA_DIRECTORY + "lineage_mutations.csv", MUTATION_TYPES).SetTimingRepeat(SYSTEMATICS_INTERVAL);
-      // AddDominantFile(*sgp_world, DATA_DIRECTORY + "dominant.csv", MUTATION_TYPES).SetTimingRepeat(SYSTEMATICS_INTERVAL);
-      // AddBestPhenotypeFile(*sgp_world, DATA_DIRECTORY+"best_phenotype.csv").SetTimingRepeat(SYSTEMATICS_INTERVAL);
-      // sgp_muller_file = emp::AddMullerPlotFile(*sgp_world, DATA_DIRECTORY + "muller_data.dat");
-      // sgp_world->OnUpdate([this](size_t ud){ if (ud % SYSTEMATICS_INTERVAL == 0) sgp_muller_file.Update(); });
-      // record_fit_sig.AddAction([this](size_t pos, double fitness) { sgp_world->GetGenotypeAt(pos)->GetData().RecordFitness(fitness); } );
-      // record_phen_sig.AddAction([this](size_t pos, const phenotype_t & phen) { sgp_world->GetGenotypeAt(pos)->GetData().RecordPhenotype(phen); } );
+      emp::AddPhylodiversityFile(*world, DATA_DIRECTORY + "phylodiversity.csv").SetTimingRepeat(SYSTEMATICS_INTERVAL);
+      emp::AddLineageMutationFile(*world, DATA_DIRECTORY + "lineage_mutations.csv", MUTATION_TYPES).SetTimingRepeat(SYSTEMATICS_INTERVAL);
+      AddDominantFile(*world, DATA_DIRECTORY + "dominant.csv", MUTATION_TYPES).SetTimingRepeat(SYSTEMATICS_INTERVAL);
+      world->OnGenotypeKnown([this](emp::Ptr<genotype_t> genotype, size_t pos) {
+        genotype->GetData().RecordMutation(last_mutation);
+      });
+      record_fit_sig.AddAction([this](size_t pos, double fitness) { world->GetGenotypeAt(pos)->GetData().RecordFitness(fitness); } );
+      record_phen_sig.AddAction([this](size_t pos, const phenotype_t & phen) { world->GetGenotypeAt(pos)->GetData().RecordPhenotype(phen); } );
       // Generate the initial population.
       do_pop_init_sig.Trigger();
     });
@@ -332,7 +343,7 @@ public:
         std::cout << "Map elites not implemented! Exiting..." << std::endl;
         exit(-1);
         break;
-      case SELECTION_METHOD_ID__ROULETTE: // TODO: will need to upshift fit functions.
+      case SELECTION_METHOD_ID__ROULETTE:
         ConfigRouletteSelection();
         break;
       default:
@@ -346,8 +357,18 @@ public:
       Agent ancestor(mid_point);
       world->SetMutFun([this](Agent & agent, emp::Random & r) {
         genome_t & genome = agent.GetGenome();
-        genome[0] = r.GetDouble(lbounds[0], ubounds[0]);
-        genome[1] = r.GetDouble(lbounds[1], ubounds[1]);
+
+        double new_x = r.GetDouble(lbounds[0], ubounds[0]);
+        double new_y = r.GetDouble(lbounds[1], ubounds[1]);
+
+
+        last_mutation["normal"] = CalcDist(genome, {new_x, new_y});
+        last_mutation["normal_x"] = emp::Abs(genome[0]-new_x);
+        last_mutation["normal_y"] = emp::Abs(genome[1]-new_y);
+
+        genome[0] = new_x;
+        genome[1] = new_y;
+
         return 2.0;
       }, 0);
       world->Inject(ancestor.GetGenome(), 1);
@@ -369,18 +390,61 @@ public:
     });
 
     // do_pop_snapshot_sig
-    do_pop_snapshot_sig.AddAction([this](size_t update) { this->Snapshot(update); });
+    do_pop_snapshot_sig.AddAction([this](size_t update) {
+      this->Snapshot(update);
+      // TODO: Tree snapshot
+    });
 
     // record_fit_sig
     // record_phen_sig
 
     // Test mutation...
     // std::cout << "(" << mid_point[0] << "," << mid_point[1] << ")" << std::endl;
-    // Agent test(mid_point);
     // for (size_t i = 0; i < 100; ++i) {
     //   Mutate(test, *random);
     //   std::cout << "(" << test.genome[0] << "," << test.genome[1] << "): " << eval_function->evaluate(test.genome) << std::endl;
     // }
+
+    // Print general info about problem.
+    // std::cout << "Lower bound: (" << lbounds[0] << "," << lbounds[1] << ")" << std::endl;
+    // std::cout << "Upper bound: (" << ubounds[0] << "," << ubounds[1] << ")" << std::endl;
+    // std::cout << "Mut stds: " << mut_std[0] << "," << mut_std[1] << std::endl;
+    // std::cout << "Mid point: (" << mid_point[0] << "," << mid_point[1] << ")" << std::endl;
+    // std::cout << "Max dist: " << max_dist << std::endl;
+    // std::cout << "Max score: " << score_ceil << std::endl;
+    // std::cout << "Floor score: " << score_floor << std::endl;
+    // std::cout << "Grid anchors: \n   ";
+    // for (size_t i = 0; i < grid_anchors[0].size(); ++i) {
+    //   std::cout << " " << grid_anchors[0][i];
+    // } std::cout << std::endl;
+    // std::cout << "   ";
+    // for (size_t i = 0; i < grid_anchors[1].size(); ++i) {
+    //   std::cout << " " << grid_anchors[1][i];
+    // } std::cout << std::endl;
+    // // Print info about key points.
+    // std::cout << "Key points: " << std::endl;
+    // for (size_t i = 0; i < key_points.size(); ++i) {
+    //   std::cout << "  " << i << ": (" << key_points[i][0] << "," << key_points[i][1] << ")" << std::endl;
+    // }
+    // // evaluate agent..
+    // do_begin_run_setup_sig.Trigger();
+    // do_evaluation_sig.Trigger();
+    // std::cout << "Printing population information" << std::endl;
+    // for (size_t id = 0; id < world->GetSize(); ++id) {
+    //   Agent & agent = world->GetOrg(id);
+    //   Phenotype & phen = agent_phen_cache[id];
+    //   // Print phenotype info about test.
+    //   std::cout << "---Phenotype "<<id<<"---" << std::endl;
+    //   std::cout << "Genome: (" << agent.genome[0] << "," << agent.genome[1] << ")" << std::endl;
+    //   std::cout << "Score: "<< phen.score << std::endl;
+    //   std::cout << "Transformed score: " << phen.transformed_score << std::endl;
+    //   std::cout << "Distances: " << std::endl;
+    //   for (size_t i = 0; i < phen.distances.size(); ++i) {
+    //     std::cout << "  (" << key_points[i][0] << "," << key_points[i][1] << ") = " << phen.distances[i];
+    //     std::cout << " ==> score = " << phen.testcase_scores[i] << std::endl;
+    //   }
+    // }
+    // exit(-1);
   }
 
   ~ToyProblemExp() {
@@ -426,6 +490,9 @@ public:
 
   void Snapshot(size_t u);
 
+  template <typename WORLD_TYPE>
+  emp::DataFile & AddDominantFile(WORLD_TYPE & world, const std::string & fpath="dominant.csv", emp::vector<std::string> mut_types = {"substitution"});
+
   void ConfigTournamentSelection();
   void ConfigLexicaseSelection();
   void ConfigEcoEASelection();
@@ -433,17 +500,27 @@ public:
 
 };
 
+/// NOTE: assumes (at least) 2 dimensions
 size_t ToyProblemExp::Mutate(Agent & agent, emp::Random & rnd) {
   genome_t & genome = agent.GetGenome();
+  for (size_t i = 0; i < MUTATION_TYPES.size(); ++i) {
+    last_mutation[MUTATION_TYPES[i]] = 0;
+  }
+  genome_t new_vals(genome.size());
   for (size_t d = 0; d < genome.size(); ++d) {
-    double new_val = rnd.GetRandNormal(genome[d], MUTATION_STD);
+    double new_val = rnd.GetRandNormal(genome[d], mut_std[d]);
     // Clamp proposed mutation value.
     new_val = (new_val > ubounds[d]) ? ubounds[d] : new_val;
     new_val = (new_val < lbounds[d]) ? lbounds[d] : new_val;
-
-    genome[d] = new_val;
+    new_vals[d] = new_val;
   }
-  return 0.0;
+  // Record last mutation info.
+  last_mutation["normal"] = CalcDist(genome, new_vals);
+  last_mutation["normal_x"] = emp::Abs(genome[0]-new_vals[0]);
+  last_mutation["normal_y"] = emp::Abs(genome[1]-new_vals[1]);
+  // Update genome with new values
+  for (size_t d = 0; d < genome.size(); ++d) { genome[d] = new_vals[d]; }
+  return genome.size();
 }
 
 void ToyProblemExp::Snapshot(size_t u) {
@@ -464,12 +541,69 @@ void ToyProblemExp::Snapshot(size_t u) {
   pop_ofstream.close();
 }
 
+template <typename WORLD_TYPE>
+emp::DataFile & ToyProblemExp::AddDominantFile(WORLD_TYPE & world, const std::string & fpath, emp::vector<std::string> mut_types) {
+  auto & file = world.SetupFile(fpath);
+
+  std::function<size_t(void)> get_update = [&world](){return world.GetUpdate();};
+  file.AddFun(get_update, "update", "Update");
+
+  std::function<double(void)> get_score = [&world, this]() {
+    Phenotype & best_phen = this->agent_phen_cache[this->best_agent_id];
+    return best_phen.score;
+  };
+  file.AddFun(get_score, "score", "get best phenotype score from this update");
+
+  for (size_t i = 0; i < mut_types.size(); ++i) {
+    std::string mut_type = mut_types[i];
+    std::function<int(void)> mut_fun = [&world, this, mut_type]() { return emp::CountMuts(world.GetGenotypeAt(this->best_agent_id), mut_type); };
+    file.AddFun(mut_fun, "dominant_"+mut_type+"_mutation_magnitude", "magnitude of "+mut_type+" mutations along dominant organism's lineage");
+  }
+
+  std::function<int(void)> dom_lin_len = [&world, this](){
+    return emp::LineageLength(world.GetGenotypeAt(this->best_agent_id));
+  };
+  std::function<int(void)> dom_lin_len_norm = [&world, this](){
+    return emp::CountMutSteps(world.GetGenotypeAt(this->best_agent_id), "normal");
+  };
+  std::function<int(void)> dom_lin_len_normx = [&world, this](){
+    return emp::CountMutSteps(world.GetGenotypeAt(this->best_agent_id), "normal_x");
+  };
+  std::function<int(void)> dom_lin_len_normy = [&world, this](){
+    return emp::CountMutSteps(world.GetGenotypeAt(this->best_agent_id), "normal_y");
+  };
+  std::function<int(void)> dom_del_step = [&world, this](){
+    return emp::CountDeleteriousSteps(world.GetGenotypeAt(this->best_agent_id));
+  };
+  std::function<size_t(void)> dom_phen_vol = [&world, this](){
+    return emp::CountPhenotypeChanges(world.GetGenotypeAt(this->best_agent_id));
+  };
+  std::function<size_t(void)> dom_unique_phen = [&world, this](){
+    return emp::CountUniquePhenotypes(world.GetGenotypeAt(this->best_agent_id));
+  };
+
+  // file.AddFun(dom_mut_count, "dominant_mutation_count", "sum of mutations along dominant organism's lineage");
+  file.AddFun(dom_lin_len, "dominant_lineage_length", "count of changes in genotype in the dominant organism's lineage.");
+  file.AddFun(dom_lin_len_norm, "dominant_lineage_length_by_normal", "count of changes in genotype due to instruction substitutions in the dominant organism's lineage.");
+  file.AddFun(dom_lin_len_normx, "dominant_lineage_length_by_normal_x", "count of changes in genotype in the dominant organism's lineage.");
+  file.AddFun(dom_lin_len_normy, "dominant_lineage_length_by_normal_y", "count of changes in genotype due to all non-tag (bit flip) mutations.");
+  file.AddFun(dom_del_step, "dominant_deleterious_steps", "count of deleterious steps along dominant organism's lineage");
+  file.AddFun(dom_phen_vol, "dominant_phenotypic_volatility", "count of changes in phenotype along dominant organism's lineage");
+  file.AddFun(dom_unique_phen, "dominant_unique_phenotypes", "count of unique phenotypes along dominant organism's lineage");
+  file.PrintHeaderKeys();
+  return file;
+
+}
+
+
 void ToyProblemExp::ConfigTournamentSelection() {
   // 1) Fill out evaluate function.
   evaluate_agent = [this](Agent & agent) {
     const size_t id = agent.GetID();
     Phenotype & phen = agent_phen_cache[id];
     phen.score = eval_function->evaluate(agent.genome);
+    record_phen_sig.Trigger(id, {phen.score});
+    record_fit_sig.Trigger(id, phen.score);
   };
   // 2) Fill out fit fun.
   fit_fun = [this](Agent & agent) {
@@ -489,11 +623,15 @@ void ToyProblemExp::ConfigLexicaseSelection() {
     const size_t id = agent.GetID();
     Phenotype & phen = agent_phen_cache[id];
     phen.score = eval_function->evaluate(agent.genome);
+    phen.transformed_score = (phen.score+score_floor)/score_ceil;
     // Evaluate distances.
     for (size_t i = 0; i < key_points.size(); ++i) {
       phen.distances[i] = CalcDist(key_points[i], agent.genome); // Don't want to end up with divide-by-zero error.
-      phen.testcase_scores[i] = (1 - phen.distances[i]/max_dist)*phen.score;
+      // phen.testcase_scores[i] = (phen.score < 0) ? (1+phen.distances[i]/max_dist)*phen.score : (1 - phen.distances[i]/max_dist)*phen.score;
+      phen.testcase_scores[i] = (1 - phen.distances[i]/max_dist) * phen.transformed_score;
     }
+    record_phen_sig.Trigger(id, {phen.score});
+    record_fit_sig.Trigger(id, phen.score);
   };
   // 2) Fill out fit fun.
   fit_fun = [this](Agent & agent) {
@@ -521,11 +659,15 @@ void ToyProblemExp::ConfigEcoEASelection() {
     const size_t id = agent.GetID();
     Phenotype & phen = agent_phen_cache[id];
     phen.score = eval_function->evaluate(agent.genome);
+    phen.transformed_score = (phen.score+score_floor)/score_ceil;
     // Evaluate distances.
     for (size_t i = 0; i < key_points.size(); ++i) {
       phen.distances[i] = CalcDist(key_points[i], agent.genome); // Don't want to end up with divide-by-zero error.
-      phen.testcase_scores[i] = (1 - phen.distances[i]/max_dist)*phen.score;
+      // phen.testcase_scores[i] = (phen.score < 0) ? (1+phen.distances[i]/max_dist)*phen.score : (1 - phen.distances[i]/max_dist)*phen.score;
+      phen.testcase_scores[i] = (1 - phen.distances[i]/max_dist) * phen.transformed_score;
     }
+    record_phen_sig.Trigger(id, {phen.score});
+    record_fit_sig.Trigger(id, phen.score);
   };
   // 2) Fill out fit fun.
   fit_fun = [this](Agent & agent) {
@@ -568,6 +710,8 @@ void ToyProblemExp::ConfigRouletteSelection() {
     const size_t id = agent.GetID();
     Phenotype & phen = agent_phen_cache[id];
     phen.score = eval_function->evaluate(agent.genome);
+    record_phen_sig.Trigger(id, {phen.score});
+    record_fit_sig.Trigger(id, phen.score);
   };
 
   // 2) Fill out fit fun.
